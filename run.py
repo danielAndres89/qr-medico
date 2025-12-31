f73cc9cb91db4bbbc3cf4ac90ebd8ec92bd0257f
Daniel Guilcapi <daniel.guilcapi@hotmail.com>
2025-12-30 19:36:27 -0500

    se suben los cambios de docker

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
+from app.extensions import db
+from flask_migrate import upgrade
 
 app = create_app()
 
+# 🔥 Ejecutar migraciones automáticamente
+with app.app_context():
+    upgrade()
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
